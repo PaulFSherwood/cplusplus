3443b3422c7154e3590ce500710ffb3d8c321324
paul <paulfsherwood@gmail.com>
2017-05-09 17:05:40 -0600

    testing a loop in QT

--- classes/classes/main.cpp
+++ classes/classes/main.cpp
@@ -1,7 +1,10 @@
 #include <QCoreApplication>
+#include <QTextStream>
+#include <QString>
 #include <iostream>
 #include "ogre.h"
 #include "display.h"
+#include <QDebug>
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -20,6 +23,23 @@ int main(int argc, char *argv[])
     t.setHealth(101);
     cout << endl;
     scene.showScene(t);
-
+    bool isRunning = true;
+    while(isRunning) {
+        QTextStream qtin(stdin);
+        QString line = qtin.readLine();  // this is how you read
+    //    QString word;
+    //    qtin >> word;
+        qDebug() << "\n LINE: " << line << endl;
+        if (line == "false") {
+            qDebug() << "rEADY TO eXiT" << endl;
+            isRunning = false;
+        } else {
+            QString str = line;
+            bool ok;
+            int numBer = str.toInt(&ok, 10);
+            t.setHealth(numBer);
+            cout << "\ngetHealth: " << t.getHealth();
+        }
+    }
     return a.exec();
 }
