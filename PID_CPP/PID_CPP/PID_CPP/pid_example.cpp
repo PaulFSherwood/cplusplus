f98bcb598ab00488d4ae2d47006cf59651a8a307
paul <2181050+PaulFSherwood@users.noreply.github.com>
2019-04-06 08:35:30 -0600

    More comments
    More comments

--- PID_CPP/PID_CPP/PID_CPP/pid_example.cpp
+++ PID_CPP/PID_CPP/PID_CPP/pid_example.cpp
@@ -1,7 +1,8 @@
 // #include <ctime>  // c++ <10
+#include <iostream>
 #include <chrono>    // g++ -std=c++0x pid_example.cpp pid.cpp -o PID
 #include <thread>
-#include <stdio.h>
+//#include <stdio.h>
 #include "pid.h"
 
 // void pause(int dur) {
@@ -10,19 +11,25 @@
 // }
 
 int main() {
+	// Setup / INIT
 	//	       dt, max,  min,  Kp,   Kd,  Ki
 	PID pid = PID(0.1, 100, -100, 0.1, 0.01, 0.5);
 	int setTemp = 0;
 	double val = 20;
+	
+	// push user temperature to setTemp variable
 	std::cout << "Please enter desired temp: ";
 	std::cin >> setTemp;
+
+	// loop for 300 * 300 milliseconds
 	for (int i = 0; i < 300; i++) {
-		double inc = pid.calculate(setTemp, val);
+		// get the current change in temp requested by the PID controller
+		double change = pid.calculate(setTemp, val);
 		printf("[%d]\tSet: %d | Reading: %7.3f | Change: %7.3f\n", i, setTemp, val, change);
-		val += inc;
-		std::this_thread::sleep_for(std::chrono::milliseconds(300)); // wait for, in milliseconds
+		val += change;
+		
+		std::this_thread::sleep_for(std::chrono::milliseconds(30)); // wait for, in milliseconds
 		// pause(1);  not used for c++ 11
 	}
-
 	return 0;
 }
