57d46d89ae649f4c60805c0f8fd35d0cb9e6ff42
PaulFSherwood <PaulFSherwood@gmail.com>
2019-04-12 22:57:35 -0600

    Moving files

---
+++ PID_CPP/PID_CPP/pid.h
@@ -0,0 +1,23 @@
+#ifndef _PID_H_
+#define _PID_H_
+
+class PIDImpl;
+class PID {
+	public:
+		// Kp - Proportional gain
+		// Ki - Integral gain
+		// Kd - derivative gain
+		// dt - loop interval time
+		// max - maximum value of manipulated variable
+		// min - minimum value of manipulated varaible
+		PID(double dt, double max, double min, double Kp, double Kd, double Ki);
+
+		// Returns the mainipulated variable given a setpoint and current process value
+		double calculate(double setpoint, double pv);
+		~PID();
+
+private:
+	PIDImpl *pimpl;
+};
+
+#endif 
---
+++ PID_CPP/PID_CPP/pid_example.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <chrono>    // g++ -std=c++0x pid_example.cpp pid.cpp -o PID
+#include <thread>
+#include "pid.h"
+
+int main() {
+	// Setup / INIT
+	//	           dt, max,  min,  Kp,   Kd,  Ki
+	PID pid = PID(0.1, 100, -100, 0.4, 0.01, 0.5);
+	int setTemp = 0;
+	double val = 20;
+	
+	// push user temperature to setTemp variable
+	std::cout << "Please enter desired temp: ";
+	std::cin >> setTemp;
+
+	// loop for 300 * 300 milliseconds
+	for (int i = 0; i < 300; i++) {
+		// get the current change in temp requested by the PID controller
+		double change = pid.calculate(setTemp, val);
+		printf("[%d]\tSet: %d | Reading: %7.3f | Change: %7.3f\n", i, setTemp, val, change);
+		val += change;
+		
+		std::this_thread::sleep_for(std::chrono::milliseconds(300)); // wait for, in milliseconds
+	}
+	return 0;
+}
