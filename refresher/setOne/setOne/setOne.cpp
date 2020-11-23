3b1f4dcb2a2976396773bf24129e9912b951fcba
Paul Sherwood <paulfsherwood@gmail.com>
2020-11-23 11:19:49 -0700

    Refersher 1-4 beginner concepts operator, objects, namespace

---
+++ refresher/setOne/setOne/setOne.cpp
@@ -0,0 +1,15 @@
+// setOne.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+
+int main()
+{
+    // the solution to libraries with the same name
+    // is to use differnt namespaces
+    // I is like having differnet containers of code to use
+    std::cout << "Hello World!\n";
+    // cout is an object that discribes the output stream.
+    // << is an operator to preform an action on to an object
+
+    printf("hey");
+}
