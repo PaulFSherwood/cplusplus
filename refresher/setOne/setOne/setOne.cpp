55c61f721937fe7ef9a753fadc86fb50ea65a21d
Paul Sherwood <paulfsherwood@gmail.com>
2020-11-23 14:33:13 -0700

    Funamental types completed.

--- refresher/setOne/setOne/setOne.cpp
+++ refresher/setOne/setOne/setOne.cpp
@@ -2,6 +2,7 @@
 //
 #include <iostream>
 #include <cmath>
+#include <climits>
 using std::cout;  // alternative to using namespace std and pulling in everthing.
 using std::cin;
 using std::endl;
@@ -17,29 +18,35 @@ int main()
     // cout is an object that discribes the output stream.
     // << is an operator to preform an action on to an object
 
-    printf("hey");
+    printf("hey\n");
 
     // C++ BASIC variable types 16,32, signed, short not included
-    char a = 'a';
-    int b = 1;
+    char a = 'a';   // char small data
+    int   b = 1;    // 16bits
+    short     z;
+    long      y;    // 32 bits
+    long long x;    // 64 bits
     float c = 1.1;
     double d = 2.2;
     bool e = false;
     // Signed numbers can be neg. or pos. but half the max value of Unsigned
+    cout << "[int: " << sizeof(b) << "][char: " << sizeof(a) << "][short: " << sizeof(z) << "][long: " << sizeof(y) << "][long long:" << sizeof(x) << "]" << endl;
 
-
+    
+    
     printVariables(a, b, c, d, e);
-    for (int i = 1; i < 5; i++) {
-
-        printVariables(a, b++, c++, d++, e);
-    }
+    // for (int i = 1; i < 5; i++) {
+    // 
+    //     printVariables(a, b++, c++, d++, e);
+    // }
     // double result = pow(10, 3);
     // cout << result << std::endl;
+
 }
 
 // function defenitions
 void printVariables(char a, int b, float c, double d, bool e) {
     printf("=====================\n");
-    cout << "a: " << a << ", b: " << b << ", c:  " << c << ", d:  " << d << ", e: " << e << endl;
+    cout << "a: " << a << ", b: " << b << ", c:  " << c << ", d:  " << d << std::boolalpha <<", e: " << e << endl;
     printf("a: %c, b: %d, c: %.2f, d: %.2f, e: %s\n\n", a, b, c, d, e ? "true" : "false");
 }
