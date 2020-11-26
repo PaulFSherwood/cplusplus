a03cea4d46d1344545b949aad0116b2c5855671b
Paul Sherwood <paulfsherwood@gmail.com>
2020-11-25 19:04:48 -0700

    The class works and can update.  The problem was a bad formula

--- refresher/setThree/encap.cpp
+++ refresher/setThree/encap.cpp
@@ -3,20 +3,19 @@
 
 class Warrior
 {
-
-public:
-    Warrior(int startLevel);   // I guess this will end up in the .h files
-    ~Warrior();
-    // I think I am doing wrong here, should this setion only be definisitons?
-    int getAgility();
-    int getStrength();
-    int getStamina();
-    int getLevel();
-    // ============================
-    void setAgility (int LEVEL);
-    void setStrength(int LEVEL);
-    void setStamina (int LEVEL);
-    void setLevel   (int LEVEL);
+    public:
+        Warrior(int startLevel);   // I guess this will end up in the .h files
+        ~Warrior();
+        // I think I am doing wrong here, should this setion only be definisitons?
+        int getAgility();
+        int getStrength();
+        int getStamina();
+        int getLevel();
+        // ============================
+        void setAgility (int LEVEL);
+        void setStrength(int LEVEL);
+        void setStamina (int LEVEL);
+        void setLevel   (int LEVEL);
 
     private:
         /* data */
@@ -27,9 +26,9 @@ public:
 };
 Warrior::Warrior(int startLevel):Level(startLevel)
 {
-    setAgility(startLevel);
+    setAgility (startLevel);
     setStrength(startLevel);
-    setStamina(startLevel);
+    setStamina (startLevel);
     std::cout << "\nAgility: " << getAgility() << "\tStrength: " << getStrength() << "\nStamina: " << getStamina() << "\tLevel: " << getLevel() <<  std::endl;
 }
 int Warrior::getAgility()    { return Agility;   }
@@ -41,19 +40,23 @@ void Warrior::setAgility(int LEVEL)
 {   // =(1/$E$2)*(EXP(($E$2+A2)/$E$3))
     const double A = 0.095;
     const double B = 22.5;
-    Agility = (1/B)*(exp((A+LEVEL)/B));
+    Agility = (1/A)*(exp((A+LEVEL)/B));
 }
 void Warrior::setStrength(int LEVEL)
 {   // =(1/$E$2)*(EXP(($E$2+A2)/$E$3))
     const double A = 0.06;
     const double B = 22.4;
-    Strength = (1/B)*(exp((A+LEVEL)/B));
+    Strength = (1/A)*(exp((A+LEVEL)/B));
+    // std::cout << "           (1/A): " << (1/A) << std::endl;
+    // std::cout << "       (A+LEVEL): " << (A+LEVEL) << std::endl;
+    // std::cout << "    (A+LEVEL)/B): " << ((A+LEVEL)/B) << std::endl;
+    // std::cout << "Current Strenght: " << Strength << std::endl;
 }
 void Warrior::setStamina(int LEVEL)
 {   // =(1/$E$2)*(EXP(($E$2+A2)/$E$3))
     const double A = 0.1;
     const double B = 22.3;
-    Stamina = (1/B)*(exp((A+LEVEL)/B));
+    Stamina = (1/A)*(exp((A+LEVEL)/B));
 }
 void Warrior::setLevel(int LEVEL)
 {
@@ -68,12 +71,15 @@ int main() {
 
 	Warrior john(10);
 
-    john.setStrength(10);
-
-    std::cout << john.getAgility() << std::endl;
-    std::cout << john.getStrength() << std::endl;
-    std::cout << john.getStamina() << std::endl;
-
-
+    // leved up
+    for (int i = 1; i < 51; i++)
+    {
+        john.setStrength(i);
+        john.setAgility(i);
+        john.setStamina(i);
+        john.setLevel(i);
+        std::cout << "\nAgility: " << john.getAgility() << "\tStrength: " << john.getStrength() << "\tStamina: " << john.getStamina() << "\tLevel: " << john.getLevel();
+    }
+    
 	return 0;
 }
