5de34fc85bbd10060c156d17eee702a355d307a7
Paul Sherwood <paulfsherwood@gmail.com>
2020-12-01 08:56:47 -0700

    Training on DFS

--- refresher/DFS.cpp
+++ refresher/DFS.cpp
@@ -1,38 +1,79 @@
 // https://www.softwaretestinghelp.com/cpp-dfs-program-to-traverse-graph/
 #include <iostream>
+#include <list>
 using namespace std;
+// graph class for dfs traversal
+class DFSGraph
+{
+    int V;  // Number of vertices
+    list<int> *adjList;                  // adjacency list
+    void DFS_util(int v, bool visited[]);  // A function used by DFS
+    public:
+        // class Constructor
+        DFSGraph(int V)
+        {
+            this->V = V;
+            adjList = new list<int>[V];
+        }
+        // function to add an edge to the graph
+        void addEdge(int v, int w)
+        {
+            adjList[v].push_back(w); // Add w to v'w list.
+        }
+        void DFS(); // DFS traversal function
+};
+void DFSGraph::DFS_util(int v, bool visited[])
+{
+    // current node v is visited
+    visited[v] = true;
+    cout << v << " ";
 
-// Step 1: Insert the root node or starting node of a tree or a graph in the stack
-// Step 2: Pop item from the stack and add it to the visited list.
-// Step 3: Find all the adjacent nodes of the node marked visited and add the ones that are not yet visited, to the stack.
-// Step 4: Repeat steps 2 and 3 until the stack is empty.
+    // recursively process all the adjacent verticies of the node
+    list<int>::iterator i;
+    for (i = adjList[v].begin(); i != adjList[v].end(); ++i)
+    {
+        if (!visited[*i])
+        {
+            DFS_util(*i, visited);
+        }
+    }
+}
 
-// Procedure DFS(G, x)
+// DFS traersl
+void DFSGraph::DFS()
+{
+    // initally none of the vertices are visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+    {
+        visited[i] = false;
+    }
 
-// G->graph to be traversed
-//
-// x->start node
-//
-// begin
-//
-// x.visited = true
-//
-// for each v   G.Adj[x]
-//
-// if v.visited == false
-//
-// DFS(G, v)
-//
-// end procedure
-//
-// init() {
-//
-// For each x   G
-//
-// x.visited = false
-//
-// For each x    G
-//
-// DFS(G, x)
-//
-// }
+    // explore the vertices one by one by recursively calling DFS_util
+    for (int i = 0; i < V; i++)
+    {
+        if (visited[i] == false)
+        {
+            DFS_util(i, visited);
+        }
+    }
+}
+
+int main()
+{
+    // creat a graph
+    DFSGraph gdfs(5);
+    gdfs.addEdge(0, 1);
+    gdfs.addEdge(0, 2);
+    gdfs.addEdge(0, 3);
+    gdfs.addEdge(1, 2);
+    gdfs.addEdge(2, 4);
+    gdfs.addEdge(3, 3);
+    gdfs.addEdge(4, 4);
+
+    cout << "Depth-first  traversal for the given graph:" << endl;
+    gdfs.DFS();
+
+    cout << endl;
+    return 0;
+}
