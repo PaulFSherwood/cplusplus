5d02139e140f3479a6a1fae520a537609269d759
Paul Sherwood <paulfsherwood@gmail.com>
2020-12-08 08:16:26 -0700

    adding in some stats

--- refresher/setThree/Warrior.cpp
+++ refresher/setThree/Warrior.cpp
@@ -3,12 +3,7 @@
 #include "Warrior.h"
 
 Warrior::Warrior(int startLevel):Level(startLevel)
-{
-    setAgility (startLevel);
-    setStrength(startLevel);
-    setStamina (startLevel);
-    std::cout << "\nAgility: " << getAgility() << "\tStrength: " << getStrength() << "\nStamina: " << getStamina() << "\tLevel: " << getLevel() <<  std::endl;
-}
+{}
 int Warrior::getAgility()    { return Agility;   }
 int Warrior::getStrength()   { return Strength;  }
 int Warrior::getStamina()    { return Stamina;   }
@@ -25,10 +20,6 @@ void Warrior::setStrength(int LEVEL)
     const double A = 0.06;
     const double B = 22.4;
     Strength = (1/A)*(exp((A+LEVEL)/B));
-    // std::cout << "           (1/A): " << (1/A) << std::endl;
-    // std::cout << "       (A+LEVEL): " << (A+LEVEL) << std::endl;
-    // std::cout << "    (A+LEVEL)/B): " << ((A+LEVEL)/B) << std::endl;
-    // std::cout << "Current Strenght: " << Strength << std::endl;
 }
 void Warrior::setStamina(int LEVEL)
 {   // =(1/$E$2)*(EXP(($E$2+A2)/$E$3))
@@ -40,6 +31,38 @@ void Warrior::setLevel(int LEVEL)
 {
     Level = LEVEL;
 }
+
+void Warrior::SetHpCap(int hpCap) { HpCap = hpCap; }
+void Warrior::SetHpCurrent(int hpCurrent) { HpCurrent = hpCurrent; }
+
+void Warrior::SetWeapon(int weapon) { Weapon = weapon; }
+void Warrior::SetArmor(int armor) { Armor = armor; }
+
+void Warrior::SetAtk(int atk) { Atk = atk; }
+void Warrior::SetDef(int def) { Def = def; }
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+int Warrior::GetHpCap() { return HpCap; }
+int Warrior::GetHpCurrent() { return HpCurrent; }
+int Warrior::GetLvl() { return Level; }
+
+int Warrior::GetWeapon() { return Weapon; }
+int Warrior::GetArmor() { return Armor; }
+
+int Warrior::GetAtk() { return Atk; }
+int Warrior::GetDef() { return Def; }
+
+void Warrior::PrintStats()
+{
+    std::cout << "HP: " <<  Warrior::GetHpCurrent() << std::endl;
+    std::cout << "Lvl: " <<  Warrior::GetLvl() << std::endl;
+    std::cout << "Wepon: " <<  Warrior::GetWeapon() << std::endl;
+    std::cout << "Armor: " <<  Warrior::GetArmor() << std::endl;
+    std::cout << "Atk: " <<  Warrior::GetAtk() << std::endl;
+    std::cout << "Def: " <<  Warrior::GetDef() << std::endl;
+}
+
+
 Warrior::~Warrior()
 {
 }
--- refresher/setThree/Warrior.h
+++ refresher/setThree/Warrior.h
@@ -17,12 +17,46 @@ class Warrior
         void setStamina (int LEVEL);
         void setLevel   (int LEVEL);
 
+
+        void SetHpCap(int hpCap);
+        void SetHpCurrent(int hpCurrent);
+        
+        void SetWeapon(int wep);
+        void SetArmor(int armor);
+
+        void SetAtk(int atk);
+        void SetDef(int def);
+        //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////
+        int GetHpCap();
+        int GetHpCurrent();
+        int GetLvl();
+
+        int GetWeapon();
+        int GetArmor();
+
+        int GetAtk();
+        int GetDef();
+
+        void PrintStats();
+
+
     private:
         /* data */
         int Agility;
         int Strength;
         int Stamina;
         int Level;
+
+        int HpCap;
+        int HpCurrent;
+
+        int Weapon;
+        int Armor;
+
+        int Atk;
+        int Def;
+
 };
 
 #endif /* WARRIOR_H_ */
