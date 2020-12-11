bbfad9fccafa19ab531a9cec45f9b4395c393052
paul <2181050+PaulFSherwood@users.noreply.github.com>
2020-12-11 15:24:15 -0700

    Not clean but works
    So I think my main problem was not using absolute correctly.  When I first used it I got an error so I stopped using subtractions in the absolute function.
    Turns out this is the answer for getting the right value.

--- refresher/setFive/bst.cpp
+++ refresher/setFive/bst.cpp
@@ -1,5 +1,3 @@
-#include <cmath> 
-
 class BST {
 public:
   int value;
@@ -11,46 +9,32 @@ public:
 };
 
 int findClosestValueInBst(BST *tree, int target) {
-	int closest = 2147483647;
-	int one, two, three = 0;
-  // keep going if you have some data
-	cout << "T: (" << target << ")\n"; 
-	while (tree != NULL)
-	{
-		if (tree        != NULL){ one     = tree->value;        } else { one     = 0; }
-		if (tree->left  != NULL){ two     = tree->left->value;  } else { two     = 0; }
-		if (tree->right != NULL){ three   = tree->right->value; } else { three   = 0; }
-		//if (tree        != NULL){ closest = tree->value;        } else { closest = 0; }
-		cout << "[" << one << "]{" << two << "}[" << three << "]=[" << closest << "]" << endl;
-		
-		// check for higher values
-		if (target > tree->value && tree->right != NULL)
+ 	int bestValue = 987654321;
+	while (tree != NULL) {
+		// cout << "tree:" << tree->value << "L:" << tree->left->value << "R:" << tree->right->value << endl;
+		// test and save the best value
+		if (abs(target - tree->value) < abs( target - bestValue)) { bestValue = tree->value; 
+		cout << "Target: " << target << " abs(tree->value): " << abs(tree->value) << " BestValue: " << bestValue << endl;
+																							 }
+		// we need to move left or right based on leafs
+		// also make sure the node isn't empty so we dont crash
+		if (target < tree->value && tree->left != NULL)
 		{
-			if ((abs(tree->value) - target) < (abs(tree->right->value) - target)) { closest = tree->value; }
-			cout << "R-V: " << target - abs(tree->value) << " R-R: " << target - abs(tree->right->value) << " c: " << closest << endl;
+			// cout << "Target: " << target << " tree->value: " << tree->value << " tree->left: " << tree->left->value << endl;
+			tree = tree->left;
+		}
+		else if (target > tree->value && tree->right != NULL)
+		{
+			// cout << "Target: " << target << " tree->value: " << tree->value << " tree->right: " << tree->right->value << endl;
 			tree = tree->right;
 		}
-		// check for lower values
-		else if (target < tree->value && tree->left != NULL)
+		// test the first node and see if it is right
+		else if (target == tree->value) { return bestValue; }
+		else
 		{
-			if ((abs(tree->value) - target) < (abs(tree->left->value) - target)) { closest = tree->value; }
-			cout << "L-V: " << abs(tree->value) - target << " R-L: " << abs(tree->left->value) - target << " c: " << closest << endl;
-			int one = abs(tree->value - target);
-			int two = abs(tree->left->value - target);
-			
-			if (one > two)
-			{
-				tree = tree->left;
-			} else {
-				return tree->value;
-			}
-		} else {
-			std::cout << "Retrun: ";
-			std::cout << tree->value << std::endl;
-			// should probably test here as i am about to output
-			// is this value clo
-			return tree->value;
+			return bestValue;
 		}
 	}
+	
   return -1;
 }
