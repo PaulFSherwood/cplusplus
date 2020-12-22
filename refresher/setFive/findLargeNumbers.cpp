594956d3752e3ed5552f3204d6b882751f2e6c1c
paul <2181050+PaulFSherwood@users.noreply.github.com>
2020-12-21 21:08:41 -0700

    This should be working.
    I really started this late, I have to try again tomorrow.
    I should be able to copy the vector, sort it, rip out everything but the last three and be done.

--- refresher/setFive/findLargeNumbers.cpp
+++ refresher/setFive/findLargeNumbers.cpp
@@ -1,15 +1,59 @@
 #include <vector>
 #include <iostream>
-#include <algorithm>
-#include <iterator>
+#include <algorithm>    // std::sort
+#include <climits>
 using namespace std;
 
+void updateLargest(vector<int> &threeLargest, int num);
+void shiftAndUpdate(vector<int> &largest, int num, int idx);
+bool myfunction (int i,int j) { return (i<j); }
+
+
+
+struct myclass {
+  bool operator() (int i,int j) { return (i<j);}
+} myobject;
+
+
 vector<int> findThreeLargestNumbers(vector<int> array) {
-	vector<int> one;
+	vector<int> newArray;
+	newArray = array;
 	
-	cout << "Max element: " << *max_element(array, array.size()) << "\n";
-	cout << "Max element location: " <<
-  distance(array, max_element(array, array.size())) << "\n";
+	sort(newArray.begin()+3, newArray.end(), myfunction);
 	
-  return {};
+	newArray.erase(newArray.end()-3,newArray.end());
+	
+	return newArray;
+	// vector<int> threeLargest{INT_MIN, INT_MIN, INT_MIN};
+	// for (int num : array)
+	// {
+	// 	updateLargest(threeLargest, num);
+	// }
+	// return threeLargest;
+}
+
+void updateLargest(vector<int> &threeLargest, int num)
+{
+	if (num > threeLargest[2])
+	{
+		shiftAndUpdate(threeLargest, num, 2);
+	} else if (num > threeLargest[1])
+	{
+		shiftAndUpdate(threeLargest, num, 1);
+	} else if (num > threeLargest[0]) {
+		shiftAndUpdate(threeLargest, num, 0);
+	}
+}
+
+void shiftAndUpdate(vector<int> &array, int num, int idx)
+{
+	for (int i = 0; i <= idx; i++)
+	{
+		if(i == idx)
+		{
+			array[i] = num;
+		} else {
+			array[i] = array[i + 1];
+		}
+	}
 }
