f8cda35753241b935298cf871b37f909e5b69dd2
Paul Sherwood <paulfsherwood@gmail.com>
2021-09-09 18:38:44 -0600

    init subsequence

---
+++ DataStructures/subsequence.cpp
@@ -0,0 +1,14 @@
+// INPUT 
+// array = [5, 1, 22, 25, 6, -1, 8, 10]
+// sequence = [1, 6, -1, 10]
+//
+// OUTPUT
+// TRUE
+using namespace std;
+
+bool isValidSubsequence(vector<int> array, vector<int> sequence) {
+  // Write your code here.
+
+  
+  return false;
+}
