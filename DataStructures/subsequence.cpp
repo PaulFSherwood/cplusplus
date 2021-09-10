996038ab0127c44cd903c9242c73920c0e456dde
Paul Sherwood <paulfsherwood@gmail.com>
2021-09-09 18:46:25 -0600

    failed test
    I am wondering how to I stop this at the last test point.

--- DataStructures/subsequence.cpp
+++ DataStructures/subsequence.cpp
@@ -9,6 +9,23 @@ using namespace std;
 bool isValidSubsequence(vector<int> array, vector<int> sequence) {
   // Write your code here.
 
-  
+    // first loop to track the first array
+    for (int i = 0; i < array.size(); i++)
+    {
+        // second loop to track the second array
+        for (int i = 0; i < sequence.size(); i++)
+        {
+            if (array[i] != sequence[j])
+            {
+                return false;
+            }
+            if (i = array.size() && array[i] == sequence[j])
+            {
+                return true;
+            }
+        }
+    }
+    
+
   return false;
 }
