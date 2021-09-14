754644578bb48e323efdc042a27f6931c7bc1767
Paul Sherwood <paulfsherwood@gmail.com>
2021-09-13 20:05:28 -0600

    Almost got it
    Almost got it.  The counter is fixing issues, but I may need to increment it on every loop

--- DataStructures/subsequence.cpp
+++ DataStructures/subsequence.cpp
@@ -8,38 +8,25 @@ using namespace std;
 
 bool isValidSubsequence(vector<int> array, vector<int> sequence) {
     bool isGud = false;
-	vector<int> vi {5, 1, 22, 25, 6, -1, 8, 10};
-	vector<int> iv {22, 25, 6};
+	int a = array.size();
+	int b = sequence.size();
+	cout << "a.size(" << array.size() << ")\tb.size(" << sequence.size() << ")\n";
+  int counter = 0;
 	
-	for (int x : vi) { cout << "x = " << x << endl;}
-	for (int y : iv) 
-	{ 
-		cout << "y = " << y << endl; 
-		if (y == iv.end()) 
-		{ 
-			cout << "ENDL\n";
-		}
-	}
-
     // first loop to track the first array
-    for (int j : sequence)
+    for (int j = 0; j < sequence.size(); j++)
     {
-			cout << "size: " << sequence.size();
+			cout << "loop(" << j << ") size: " << sequence.size();
 				cout << endl;
 			isGud = false;
         // second loop to track the second array
-        for (int i : array)  // .size()-1 to not run off the end of the array
+        for (int i = counter; i < array.size(); i++)  // .size()-1 to not run off the end of the array
         {
-					  cout << "sequence[" << j << "] = " << sequence[j] << endl;
-					  cout << "array["    << i << "] = " << array[i]    << endl;
+					  cout << "s[" << j << "] = " << sequence[j];
+					  cout << "\ta["    << i << "] = " << array[i]    << endl;
 					
 					  if (sequence[j] == array[i]) { cout << sequence[j] << endl; }
 					
-						// causes one run failure
-						// if (array[i] != sequence[j])
-						// {
-						// 		return false; 
-						// }
 						// if we finished the loop and found nothing, then everything should fail
 						// this should go first in case the next test passes.
 					  if (i == array.size()-1 && array[i] != sequence[j])
@@ -52,13 +39,12 @@ bool isValidSubsequence(vector<int> array, vector<int> sequence) {
             if (array[i] == sequence[j])
             {
 							cout << "GOOOOOOOOOOOOOOOOOD\n";
-                isGud = true;
-							// j++;
+              isGud = true;
+							counter++;
 							// i=0;
+							break;
             }
         }
     }
-    
-
   return isGud;
 }
