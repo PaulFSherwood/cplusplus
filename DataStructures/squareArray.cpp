3166f7ae256541ef21d9fb9a100521502795ca8b
Paul Sherwood <paulfsherwood@gmail.com>
2021-09-14 19:42:41 -0600

    Works, squares and sorts
    The sort is probably supposed to be done by hand.

--- DataStructures/squareArray.cpp
+++ DataStructures/squareArray.cpp
@@ -5,16 +5,23 @@
 // array = [1, 4, 9, 25, 36, 64, 81]
 
 #include <vector>
+#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
 vector<int> sortedSquaredArray(vector<int> array) {
   // Write your code here.
 	vector<int> muhArray;
+	int holder = 0;
+	// set power 2 for the vector
 	for (int i : array)
 	{
-		muhArray.push_back(i);
-		cout << "I: " << i << " ";
+		holder = i;
+		muhArray.push_back(pow(holder, 2));
+		cout << "I: " << holder << "\t I2: " << pow(holder, 2) << endl;
+		holder = 0;
 	}
 	cout << endl;
+	sort(muhArray.begin(), muhArray.end());  // they probably want me to do this by hand.
   return muhArray;
 }
