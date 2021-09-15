1e136548c346dda44e613a27923c242a504ecc76
Paul Sherwood <paulfsherwood@gmail.com>
2021-09-14 19:31:56 -0600

    First run

--- DataStructures/squareArray.cpp
+++ DataStructures/squareArray.cpp
@@ -1,7 +1,20 @@
+// INPUT
+// array = [1, 2, 3, 5, 6, 8, 9]
+// 
+// OUTPUT
+// array = [1, 4, 9, 25, 36, 64, 81]
+
 #include <vector>
 using namespace std;
 
 vector<int> sortedSquaredArray(vector<int> array) {
   // Write your code here.
-  return {};
+	vector<int> muhArray;
+	for (int i : array)
+	{
+		muhArray.push_back(i);
+		cout << "I: " << i << " ";
+	}
+	cout << endl;
+  return muhArray;
 }
