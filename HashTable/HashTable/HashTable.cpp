c4a2493b9f7c25831bc805b62555ac30d55da1d2
Paul Sherwood <paulfsherwood@gmail.com>
2021-09-20 19:18:08 -0600

    Hash working
    Hash table is working, need to read back over it.
    
    "auto& cell = table[hashValue];"

--- HashTable/HashTable/HashTable.cpp
+++ HashTable/HashTable/HashTable.cpp
@@ -73,5 +73,49 @@ void HashTable::removeItem(int key) {
 }
 
 void HashTable::printTable() {
+	for (int i{}; i < hashGroups; i++) {
+		if (table[i].size() == 0) continue;
+		auto bItr = table[i].begin();
+		for (; bItr != table[i].end(); bItr++) {
+			cout << "[INFO] Key: " << bItr->first << " Value: " << bItr->second << endl;
+		}
+	}
+	return;
+}
 
+int main() {
+
+	HashTable HT;
+
+	if (HT.isEmpty()) {
+		cout << "Correct answer.  Good job.";
+	}
+	else {
+		cout << "Oh oh.  We need to check out our code!" << endl;
+	}
+
+	HT.insertItem(905, "Jim");
+	HT.insertItem(201, "Debrah");
+	HT.insertItem(332, "David");
+	HT.insertItem(124, "Ether");
+	HT.insertItem(107, "Sandy");
+	HT.insertItem(929, "Barb");
+	HT.insertItem(928, "Rob"); 
+	HT.insertItem(928, "Bob");
+
+	HT.printTable();
+
+	HT.removeItem(332);
+	HT.removeItem(100);
+
+	if (HT.isEmpty()) {
+		cout << "Oh oh.  We need to check out our code!" << endl;
+	}
+	else {
+		cout << "Correct answer.  Good job.";
+	}
+
+	HT.printTable();
+
+	return 0;
 }
