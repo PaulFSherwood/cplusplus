23fae3e5294d240d7bd4aee0a9c29c4ef0cc8b92
Paul Sherwood <paulfsherwood@gmail.com>
2021-09-25 19:47:46 -0600

    I don't like this solution
    I think this must have a better looking solution.
    It is right, but it looks off for some reason.

--- DataStructures/fib.cpp
+++ DataStructures/fib.cpp
@@ -3,6 +3,31 @@
 #include <iostream>
 using namespace std;
 
+// for memo  // from stackoverflow "fib memoization algo
+#define N 100
+const int NIL = -1;
+int lookup_table[N];
+
+void init()
+{
+    for (int i = 0; i < N; i++)
+    {
+        lookup_table[i] = NIL;
+    }
+}
+
+int fib_mem(int n) {
+    if(lookup_table[n] == NIL) {
+        if(n <= 1)
+        {
+            lookup_table[n] = n;
+        } else {
+            lookup_table[n] = fib_mem(n-1) + fib_mem(n-2);
+        }
+    }
+    return lookup_table[n];
+}
+
 // siple fib function
 int fib(int n)
 {
