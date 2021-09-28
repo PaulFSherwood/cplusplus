9c0fef724ca86370b3cb8626cd8c1e305e127ff2
Paul Sherwood <paulfsherwood@gmail.com>
2021-09-27 19:47:40 -0600

    Testing hash tables

---
+++ HashTable/sample.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <list>
+#include <cstring>
+using namespace std;
+
+class HashTable {
+    private:
+    static const int hashGroup = 10;
+    list <pair<int, string>> table[hashGroups];
+
+    public:
+    bool isEmpty() const;
+    int hashFunction(int key);
+    void insertItem(int key, string value);
+    void removeItem(int key);
+    string searchTable(int key);
+    void printTable();
+}
