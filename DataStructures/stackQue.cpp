f50a2c0214dbb12337904f127fd701732ff6fdd5
Paul Sherwood <paulfsherwood@gmail.com>
2021-10-03 20:14:19 -0600

    Adding more options
    from page 228 of cracking the coding interview

--- DataStructures/stackQue.cpp
+++ DataStructures/stackQue.cpp
@@ -15,4 +15,40 @@ class FixedMultiStack {
         /* check that we have space for the next elemnet */
         if (isFull(stakcNum)) {
             throw new FullStackException();
+        }
+        /* pop item fromto stack */
+        public int pop(int stackNum) {
+            if (isEmpty(stackNum)) {
+                throw new EmptyStackException();
+            }
+            int topIndex = intdedOfTop(stakcNum);
+            int value = values[topIndex]; // Get top
+            values[topIndex] = 0; // clear
+            sizes[stackNum]--; // shrink
+            return value;
+        }
 
+        // Retrn top element.
+        public int peek(int stackNum) {
+            if(isEmpty(stackNum)) {
+                throw new EmptyStackException();
+            }
+            return values[indexOfTop(stackNum)];
+        }
+
+        /* Return if stakc is empty */
+        public boolean isEmpty(int stackNum) {
+            reutnr sizes[stackNum] == 0;
+        }
+
+        /*  Return if stack is full. */
+        public boolean isFull(int stackNum) {
+            return sizes[stackNum] == stackCapacity;
+        }
+
+        /* Return index of the top of th stack */
+        private int indexOfTop(int stackNum) {
+            int offset = stackNum * stackCapacity;
+            int size = sizes[stackNum];
+            return offset + size - 1;
+        }
