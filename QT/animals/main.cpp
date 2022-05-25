22d6cced6f4fe579057b4e4b894b1bfb3a9f8117
PaulFSherwood <paulfsherwood@gmail.com>
2022-05-25 10:59:43 -0600

    inheritance test

---
+++ QT/animals/main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include "feline.h"
+#include "canine.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Canine dog;
+    Feline cat;
+
+    return a.exec();
+}
