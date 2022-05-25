0fb824a05982fb050651d0a871a8363eb8f64b4b
PaulFSherwood <paulfsherwood@gmail.com>
2022-05-25 11:02:39 -0600

    inheritance test(multiple inheritance failure)

--- QT/animals/main.cpp
+++ QT/animals/main.cpp
@@ -2,6 +2,11 @@
 #include "feline.h"
 #include "canine.h"
 
+/*
+ * From VoidRealms
+ * https://github.com/voidrealms/qt6-core-beginners/tree/main/section%208/qt6cb-8-4
+ */
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
