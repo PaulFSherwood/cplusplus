b46db8b74394e8df00eedf8f488ecd779791bb5a
PaulFSherwood <paulfsherwood@gmail.com>
2025-09-17 17:28:46 -0600

    Just started CPP refresher

---
+++ antiRTFM/2025refresher/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Section 1
+	// cout << "Here is some text \n";
+	// cout << "Here is a number: ";
+	// cout << 10 << endl;
+
+	// cout << "10 + 7 = " << 10+7 << endl;
+	// 
+	// int x, y = 0;
+	// x = 7;
+	// y = 10;
+
+	// cout << x << " + " << y << " = " << x+y << endl;
+
+	// Section 2
+	float a = 5.1234567890;  // 5.123456
+	double b = 6.1234567890; // 6.123456
+
+	cout << "a:" << a << "\nb:" << b << endl;
+
+
+	return 0;
+}
