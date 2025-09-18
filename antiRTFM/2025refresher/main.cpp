ae438fab171816ed7f3272ba5c0c14deea50c3e5
PaulFSherwood <paulfsherwood@gmail.com>
2025-09-17 19:11:22 -0600

    started functions

--- antiRTFM/2025refresher/main.cpp
+++ antiRTFM/2025refresher/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// Section 3
+void myFunction();
+void myFunction2();
+int getInteger();
+
 int main()
 {
 	// Section 1
@@ -17,11 +22,71 @@ int main()
 	// cout << x << " + " << y << " = " << x+y << endl;
 
 	// Section 2
-	float a = 5.1234567890;  // 5.123456
-	double b = 6.1234567890; // 6.123456
+	// float a = 5.1234567890;  // 5.123456
+	// double b = 6.1234567890; // 6.123456
 
-	cout << "a:" << a << "\nb:" << b << endl;
+	// cout << "a:" << a << "\nb:" << b << endl;
 
+	// int x = 3;    //   3
+	// char y = 'u'; // 117
+	// cout << x+y <<endl;
 
+	// if (x == 3)
+	// {
+	// 	cout << "X is three" << endl;
+	// }
+	// 
+	// int  number = 76;
+	// int  playerNumber = 0;
+	// bool isRunning = true;
+	// int  maxGuesses = 10;
+
+	// while (isRunning)
+	// //while(isRunning || playerNumber != number)
+	// {
+	// 	cout << "Guess a number: ";
+	// 	cin >> playerNumber;
+	// 	if(playerNumber == number)
+	// 	{
+	// 		isRunning = false;
+	// 		cout << "You guessed the number" << endl;
+	// 	}
+	// 	else
+	// 	{
+	// 		if (playerNumber <= number)
+	// 		{
+	// 			cout << "Your number is too low: " << maxGuesses << " left" << endl;
+	// 		}
+	// 		if (playerNumber >= number)
+	// 		{
+	// 			cout << "Your number is too high " << maxGuesses << " left" << endl;
+	// 		}
+	// 	}
+	// 	maxGuesses -= 1;
+	// 	if(maxGuesses <= 0)
+	// 	{
+	// 		isRunning = false;
+	// 	}
+	// }
+	
+	// Section 3
+	myFunction();
+	getInteger();
+	cout << getInteger() << endl;
 	return 0;
+}
+
+// Section 3
+void myFunction()
+{
+	cout << "My function" << endl;
+	myFunction2();
+}
+void myFunction2()
+{
+	cout << "My function2" << endl;
+}
+int getInteger()
+{
+	return 5;
 }
