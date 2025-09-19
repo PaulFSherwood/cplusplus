fc668881e9554ba485a2b0b6e941fd0f94c1b35c
PaulFSherwood <PaulFSherwood@gmail.com>
2025-09-19 17:25:57 -0600

    Testing static class elements

--- antiRTFM/2025refresher/main.cpp
+++ antiRTFM/2025refresher/main.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
 #include <random>  // For rand() and srand()
+#include <string>
 using namespace std;
 
 // Section 4
 int getInteger();
 
-class Ogre
-{
+class Character {
     public:
-        void attack();
-        void setHealth(int hp);
-        void setStrength(int str);
-    private:
-        int health;
-        int strength;        
+        std::string name;
+        int health{};
+        int attack{};
+        int defense{};
+
+        // General Constructor
+        explicit Character(const std::string& n, int h, int a, int d)
+            : name(n), health(h), attack(a), defense(d) {}
+
+        // Factory: hero
+        static Character Hero(const std::string& n, int h = 50, int a = 5, int d = 5) {
+            return Character(n, h, a, d);
+        }
+        // Factory: Monster
+        static Character Monster(const std::string& n, int h = 100, int a = 10, int d = 10) {
+            return Character(n, h, a, d);
+        }
+
+        // Friend for printing
+        friend std::ostream& operator<<(std::ostream& os, const Character& c) {
+            return os << '"' << c.name << "\", "
+                      << c.health << ", "
+                      << c.attack << ", "
+                      << c.defense;
+        }
 };
 
 int main()
 {
-    Ogre me;
-	cout << getInteger();
-    cout << endl;
-    me.setHealth(10);
-    me.setStrength(10);
-	me.attack();
+    auto hero   = Character::Hero("Arthur");
+    auto goblin = Character::Monster("Goblin", 100, 50);
+    auto ogre = Character::Monster("Ogre", 200, 100);
+
+    std::cout << hero << "\n";
+    std::cout << goblin << "\n";
+    std::cout << ogre << "\n";
 
     return 0;
 }
@@ -35,15 +55,3 @@ int getInteger()
 	return dist(gen);
 }
 
-void Ogre::attack()
-{
-    cout << "Ogre attack with " << strength << endl;
-};
-void Ogre::setHealth(int hp)
-{
-    health = hp;
-};
-void Ogre::setStrength(int str)
-{
-    strength = str;
-};
