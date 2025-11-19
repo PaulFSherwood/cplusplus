1fb1c5d5c6b050e698751c2100a59021e4034a46
PaulFSherwood <paulfsherwood@gmail.com>
2025-11-19 12:02:08 -0700

    thread updated and folder clean up

---
+++ threads/main.cpp
@@ -0,0 +1,23 @@
+// @file main.cpp
+// g++ -std=c++17 main.cpp -o prog -lpthread
+#include <iostream>
+#include <thread>   // Include the thread library
+
+// Test function which we'll launch threads from
+void test(int x) {
+   std::cout << "Hello from thread!" << std::endl;
+   std::cout << "Argument passed in: " << x << std::endl;
+}
+
+int main() {
+   // Create a new thread and pass one parameter
+   std::thread myThread(&test, 100);
+   // Join with the main thread, which is the same as
+   // saying "hey, main thread--wait until myThread
+   //         finishes before executing further."
+   myThread.join();
+   // Continue executing the main thread
+   std::cout << "Hello from my main thread" << std::endl;
+
+   return 0;
+}
