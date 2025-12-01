7fd2b3f82d19dd5158e7229acda787340663de8c
PaulFSherwood <paulfsherwood@gmail.com>
2025-12-01 12:02:44 -0700

    setup moving folders around prior to c++ reading

---
+++ modernCPP/02/main.cpp
@@ -0,0 +1,13 @@
+#include <print>
+
+int main() {
+   int    a   =       4;
+   double b   =     3.0;
+   char c[]   = "hello";
+
+   std::print("Hello World!\n");
+
+   std::print("Hello World!\n {}, {}, {}, {}\n", a, b, c, "Hey Paul");
+
+   return 0;
+}
