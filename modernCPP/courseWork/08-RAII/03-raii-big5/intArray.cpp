477a938d5707c7e3857b3c74f77d6c2559805c7f
PaulFSherwood <PaulFSherwood@gmail.com>
2025-12-13 15:33:11 -0700

    Move assignment overloading

--- modernCPP/courseWork/08-RAII/03-raii-big5/intArray.cpp
+++ modernCPP/courseWork/08-RAII/03-raii-big5/intArray.cpp
@@ -14,7 +14,7 @@ IntArray::~IntArray() {
 }
 // Copy constructor policy
 IntArray::IntArray(const IntArray &rhs) {
-    m_name = rhs.m_name;
+    m_name = rhs.m_name+"(copy)";
     std::cout << " was copy constructed from: " << rhs.m_name << std::endl;
     m_data = new int[10];
     for (int i = 0; i < 10; i++) {
@@ -25,12 +25,31 @@ IntArray::IntArray(const IntArray &rhs) {
 IntArray &IntArray::operator=(const IntArray &rhs) {
     if (this != &rhs) {
         delete[] m_data;
-        m_name = rhs.m_name;
+        m_name = rhs.m_name+"(copy)";
         std::cout << " was copy assigned from" << rhs.m_name << std::endl;
         m_data = new int[10];
         for (int i = 0; i < 10; i++) {
             m_data[i] = rhs.m_data[i];
         }
+    }
+    return *this;
+}
+// Move constructor
+IntArray::IntArray(IntArray &&source) {
+    m_name = source.m_name;
+    source.m_name = "";
+    m_data = source.m_data;
+    source.m_data = nullptr;
+    std::cout << m_name << " was move constructed" << std::endl;
+}
+// Move assignment operator
+IntArray &IntArray::operator=(IntArray &&source) {
+    if (this!=&source) {
+        m_name = source.m_name;
+        source.m_name = "";
+        m_data = source.m_data;
+        source.m_data = nullptr;
+        std::cout << m_name << " used move assignment" << std::endl;
     }
     return *this;
 }
--- modernCPP/courseWork/08-RAII/03-raii-big5/main.cpp
+++ modernCPP/courseWork/08-RAII/03-raii-big5/main.cpp
@@ -2,8 +2,22 @@
 #include <vector>
 #include "intArray.hpp"
 
+IntArray foo() {
+    IntArray result("foo() created array");
+    return result;
+}
+
 int main() {
-    IntArray array1("array1");
+    // IntArray array1("array1");
+    // Copy made
+    // IntArray array2 = array1;
+    //IntArray array2 = foo();
+    std::vector<IntArray> myArrays;
+    myArrays.reserve(10);   // reserving memory ahead of time. Not needed. 35 vs 13 allocs 
+    for (int i = 0; i < 10; i++) {
+        IntArray temp(std::to_string(i));
+        myArrays.push_back(std::move(temp));
+    }
 
     return 0;
 }
