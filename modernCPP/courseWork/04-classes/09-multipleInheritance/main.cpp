f61e1a5e180dc7ec8969e9cf87607fcf8006290e
PaulFSherwood <PaulFSherwood@gmail.com>
2025-12-14 12:54:21 -0700

    inheritance update

--- modernCPP/courseWork/04-classes/09-multipleInheritance/main.cpp
+++ modernCPP/courseWork/04-classes/09-multipleInheritance/main.cpp
@@ -4,11 +4,11 @@ struct Entity {
     int x{4},y{5};
 };
 
-struct Warrior : public Entity {
+struct Warrior : virtual public Entity {
     int power{10};
 };
 
-struct Goblin : public Entity {
+struct Goblin : virtual public Entity {
     int power{7};
 };
 
@@ -16,14 +16,14 @@ struct Player : public Goblin, public Warrior {
 };
 
 int main() {
+
     Player mike;
-    std::cout << mike.Warrior::x << std::endl;
-    std::cout << mike.Warrior::y << std::endl;
+    std::cout << mike.x << std::endl;
+    std::cout << mike.y << std::endl;
     
     std::cout << mike.Goblin::x << std::endl;
     std::cout << mike.Goblin::y << std::endl;
     std::cout << mike.Goblin::power << std::endl;
 
     return 0;
-
 }
