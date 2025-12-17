69e7ce08d63ba67cfb6ec4612e1303b641ab6a32
PaulFSherwood <paulfsherwood@gmail.com>
2025-12-17 12:31:46 -0700

    more member variables and static members.

--- modernCPP/courseWork/10-storage/02-memberVariables/main.cpp
+++ modernCPP/courseWork/10-storage/02-memberVariables/main.cpp
@@ -4,15 +4,22 @@ struct API {
    API() {}
    ~API() {}
 
+   // Static variables in a class exists outside of the class.
+   // The scope is within the class.
    static int MAJOR;
    static int MINOR;
 
 };
 
+int API::MAJOR = 7;
+
 int main() {
 
-   API instance;
-   instance.MAJOR = 7;
+   // API instance;
+   // instance.MAJOR = 7;
 
+   std::cout << "Major: " << API::MAJOR << std::endl;
+   API::MAJOR = 9;
+   std::cout << "Major: " << API::MAJOR << std::endl;
    return 0;
 }
