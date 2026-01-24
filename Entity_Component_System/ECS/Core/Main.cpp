a48d884fcd7cdbb9576f65a2c4df411746704f01
PaulFSherwood <PaulFSherwood@gmail.com>
2026-01-24 12:17:53 -0700

    commenting main

--- Entity_Component_System/ECS/Core/Main.cpp
+++ Entity_Component_System/ECS/Core/Main.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-    Application app;
-    app.Init();
-    app.Run();
-    app.Shutdown();
+    Application app;    // Creates the App with private variables and functions
+    app.Init();         // Starts the Init function : Sets up the video screen, starts adding the player with entities. Sets the player to initial location 
+    app.Run();          // Starts the Run function : Adds in the input, movement, render and collision systems : then updates each.
+    app.Shutdown();     // Cleanly kills Renderer, Window, and Quits SDL.
     return 0;
 }
 
