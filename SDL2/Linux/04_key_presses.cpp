a274b879ec6fca7c7b2c05640ea0b3d33637040d
sherwoodpa <paulfsherwood@gmail.com>
2026-05-20 22:46:30 -0600

    testing from Lazy Foo's tutorials.

---
+++ SDL2/Linux/04_key_presses.cpp
@@ -0,0 +1,195 @@
+/* This source code copyrighted by Lazy Foo' Productions (2004-2015)
+ * and may not be redistributed without written permission.*/ 
+
+//Using SDL, standard IO, and strings 
+#include <SDL.h>
+#include <stdio.h>
+#include <string>
+#ifdef _JS
+#include <emscripten.h>
+#endif
+// Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+// Key press surfaces constants
+enum KeyPressSurfaces
+{
+  KEY_PRESS_SURFACE_DEFAULT,
+  KEY_PRESS_SURFACE_UP,
+  KEY_PRESS_SURFACE_DOWN,
+  KEY_PRESS_SURFACE_LEFT,
+  KEY_PRESS_SURFACE_RIGHT,
+  KEY_PRESS_SURFACE_TOTAL 
+};
+
+// Starts up SDL and creates the window
+bool init();
+
+// Loads media 
+bool loadMedia();
+
+// Frees media and shuts down SDL 
+void close();
+
+// Loads individual image 
+SDL_Surface* loadSurface( std::string path );
+
+// The window we'll be rendering to 
+SDL_Window* gWindow = NULL;
+
+// The surface contained by the window 
+SDL_Surface* gScreenSurface = NULL; 
+
+// The images that correspond to a keypress 
+SDL_Surface* gKeyPressSurface[ KEY_PRESS_SURFACE_TOTAL ];
+
+// Current displayed image 
+SDL_Surface* gCurrentSurface = NULL;
+
+bool init()
+{
+  // initialization flag 
+  bool success = true;
+
+  // Initialize SDL 
+  if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+  {
+    printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+    success = false;
+  }
+  else 
+  {
+    // Create window
+    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if ( gWindow == NULL )
+    {
+      printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+      success = false;
+    }
+    else
+    {
+      // Get window surface 
+      gScreenSurface = SDL_GetWindowSurface( gWindow );
+    }
+  }
+
+  return success;
+}
+
+void close()
+{
+	//Deallocate surfaces
+	for( int i = 0; i < KEY_PRESS_SURFACE_TOTAL; ++i )
+	{
+		SDL_FreeSurface( gKeyPressSurfaces[ i ] );
+		gKeyPressSurfaces[ i ] = NULL;
+	}
+
+	//Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+SDL_Surface* loadSurface( std::string path )
+{
+	//Load image at specified path
+	SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+	}
+
+	return loadedSurface;
+}
+
+
+//Main loop flag
+bool quit = false;
+
+void loop_handler(void*)
+{
+	//Event handler
+	SDL_Event e;
+ 	//Handle events on queue
+	while( SDL_PollEvent( &e ) != 0 )
+	{
+		//User requests quit
+		if( e.type == SDL_QUIT )
+		{
+			quit = true;
+		}
+//User presses a key
+		else if( e.type == SDL_KEYDOWN )
+		{
+			//Select surfaces based on key press
+			switch( e.key.keysym.sym )
+			{
+				case SDLK_UP:
+					gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
+					break;
+				case SDLK_DOWN:
+					gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ];
+					break;
+				case SDLK_LEFT:
+					gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ];
+					break;
+				case SDLK_RIGHT:
+					gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ];
+					break;
+				default:
+					gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
+					break;
+			}
+		}
+	}
+
+	//Apply the image
+	SDL_BlitSurface( gCurrentSurface , NULL, gScreenSurface, NULL );
+			
+	//Update the surface
+	SDL_UpdateWindowSurface( gWindow );
+}
+
+int main( int argc, char* args[] )
+{
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{	
+
+			//Set default current surface
+			gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
+
+#ifdef _JS
+
+                        emscripten_set_main_loop_arg(loop_handler, NULL, -1, 1);
+#else
+			//While application is running
+			while( !quit )
+			{
+		 	 loop_handler(NULL);	
+			}
+#endif
+
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
