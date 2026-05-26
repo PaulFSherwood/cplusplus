81210cd7f1f1b92a5e1edfec393425952f0bc6a7
sherwoodpa <paulfsherwood@gmail.com>
2026-05-26 15:20:18 -0600

    SDL3 testing

---
+++ sdl3/beginner/main.cpp
@@ -0,0 +1,71 @@
+#include <SDL3/SDL.h>
+#include <iostream>
+
+int main()
+{
+  if (!SDL_Init(SDL_INIT_VIDEO))
+  {
+    std::cout << "SDL_Init failed: " << SDL_GetError() << std::endl;
+  }
+
+  SDL_Window* window = SDL_CreateWindow(
+      "SDL3 Lesson 1 = Drawing",
+      800,
+      600,
+      0
+      );
+
+  if (window == nullptr)
+  {
+    std::cout << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+  if (renderer == nullptr)
+  {
+    std::cout << "SDL_CreateRenderer failed: " << SDL_GetError() << std::endl;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  bool running = true;
+
+  while (running)
+  {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_EVENT_QUIT)
+      {
+        running = false;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 20, 20, 30, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_FRect box;
+    box.x = 350.0f;
+    box.y = 250.0f;
+    box.w = 100.0f;
+    box.h = 100.0f;
+
+    SDL_SetRenderDrawColor(renderer, 200, 80, 80, 255);
+    SDL_RenderFillRect(renderer, &box);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(16);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
